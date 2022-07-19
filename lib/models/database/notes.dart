870a4a025d59a45e291d5b5103ab4f7eb44d3f7b
Alexander Adam <alexanderadamm21@gmail.com>
2022-07-19 23:01:27 +0700

    migrate to Drift

--- lib/models/database/notes.dart
+++ lib/models/database/notes.dart
@@ -1,56 +1,72 @@
-import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 import '../note.dart';
 
-class NotesDatabase {
-  final _name = "NotesDatabase.db";
-  final _version = 1;
+part 'notes.g.dart';
 
-  late Database database;
-  final tableName = 'notes';
+@DataClassName('SavedNote')
+class SavedNotes extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  TextColumn get content => text()();
+}
 
-  initDatabase() async {
-    database = await openDatabase(_name, version: _version,
-        onCreate: (Database db, int version) async {
-      await db.execute('''CREATE TABLE $tableName (
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            title TEXT,
-            content TEXT)''');
-    });
-  }
+@DriftDatabase(tables: [SavedNotes])
+class MyDatabase extends _$MyDatabase {
+  // we tell the database where to store the data with this constructor
+  MyDatabase() : super(_openConnection());
+
+  // you should bump this number whenever you change or add a table definition.
+  // Migrations are covered later in the documentation.
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'notes.db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+
+@DriftAccessor(tables: [SavedNotes])
+class DbQueries extends DatabaseAccessor<MyDatabase> with _$DbQueriesMixin {
+  DbQueries(MyDatabase db) : super(db);
 
   Future<int> insertNote(Note note) async {
-    return await database.insert(tableName, note.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
+    return await into(savedNotes).insert(
+      SavedNotesCompanion(
+          title: Value(note.title), content: Value(note.content)),
+    );
   }
 
   Future<int> updateNoteById(Note note) async {
-    return await database.update(tableName, note.toMap(),
-        where: 'id = ?',
-        whereArgs: [note.id],
-        conflictAlgorithm: ConflictAlgorithm.replace);
+    return await (update(savedNotes)..where((t) => t.id.equals(note.id))).write(
+      SavedNotesCompanion(
+          title: Value(note.title), content: Value(note.content)),
+    );
   }
 
-  Future<List<Map<String, dynamic>>> getAllNotes() async {
-    return await database.query(tableName);
+  Future<List<SavedNote>> getAllNotes() async {
+    return await (select(savedNotes)
+          ..orderBy([(t) => OrderingTerm(expression: t.title)]))
+        .get();
   }
 
-  Future<Map<String, dynamic>?> getNoteById(int id) async {
-    var result =
-        await database.query(tableName, where: 'id = ?', whereArgs: [id]);
-
-    if (result.isEmpty) {
-      return null;
-    }
-
-    return result.first;
+  Future<List<SavedNote>> getNoteById(int id) async {
+    return await (select(savedNotes)..where((t) => t.id.equals(id))).get();
   }
 
   Future<int> deleteNoteById(int id) async {
-    return await database.delete(tableName, where: 'id = ?', whereArgs: [id]);
-  }
-
-  closeDatabase() async {
-    await database.close();
+    return await (delete(savedNotes)..where((t) => t.id.equals(id))).go();
   }
 }
